ef5035b5e1ba6cf18ce4b7c723a4684d1f2f1489
bjgman12 <bjgman38@gmail.com>
2020-10-15 22:15:45 -0600

    all day one features working

--- README.md
+++ README.md
@@ -2,12 +2,41 @@
 
 **Author**: Brandon Gonzalez
 
-**Version** 1.0.1
+**Version** 1.0.2
 
 ## Overview
 
 >This is a server running for a City Explorer app connecting it to APIs to provide users info about requested cities
 
+### Feature One Map Data
+
+- estimated time 30 minutes
+
+- start time 7pm
+
+-end time 8pm
+
+actual time 1hr
+
+### Feature Two Weather Data
+
+- estimated time 1 hr
+
+- start time 8:30
+
+-end time 9:00
+
+-actual time 30 minutes
+
+### Feature three Error handling
+
+- esimtated time 1 hr
+
+- start time 9:30
+
+- end time 10:00
+
+-actual time 30 minutes
 ## Getting Startded
 
 - Server init
--- server.js
+++ server.js
@@ -27,23 +27,57 @@ app.get('/', (request, response) => {
   response.send('Default');
 });
 
-app.get('/location', (request,response) => {
-  let city = request.query.city;
-  let data = require('data/location.json')[0];
-  let location = new Location(data,city);
-  response.send(location);
+app.get('/bad', (request,response) => {
+  throw new Error('Wrong');
 });
 
+app.get('/location', (request, response) => {
+  try {
+    let data = require('./data/location.json')[0];
+    let city = request.query.city;
+    let location = new Location(data, city);
+    console.log(location);
+    response.send(location);
+  }
+  catch (error) {
+    console.log('Error', error);
+    response.status(500).send('So sorry, something went wrong.');
+  }
+});
+
+app.get('/weather', (request, response) => {
+  try {
+    let data = require('./data/weather.json');
+    let weatherArr = [];
+    data.data.forEach(day => {
+      let weather = new Weather(day);
+      weatherArr.push(weather);
+    });
+    response.send(weatherArr);
+  }
+  catch (error) {
+    console.log('Error', error);
+    response.status(500).send('So sorry,something went wrong');
+  }
+});
 
 // Constructors
 
-function Location(obj,query){
-  this.lat = obj.lat;
-  this.lon = obj.lon;
+function Location(obj, query) {
+  this.latitude = obj.lat;
+  this.longitude = obj.lon;
   this.search_query = query;
-  this.location = obj.display.name;
+  this.formatted_query = obj.display_name;
 }
 
+function Weather(obj) {
+  this.forecast = obj.weather.description;
+  this.time = obj.datetime;
+
+}
+
+
+//Starting Server
 app.listen(PORT, () => {
   console.log(`server is now listening on ${PORT}`);
 });
